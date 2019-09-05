e6644d47649e97c805c0b80bb25d9e3e7e104faa
Ivan Notaros <nothke@gmail.com>
2019-09-06 00:36:42 +0200

    Added UI pie and slider

---
+++ NDraw/DrawPie.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NDraw
+{
+    public static partial class Draw
+    {
+        public static partial class Screen
+        {
+            public static void Pie(int x, int y, float innerRadius, float outerRadius, float value)
+            {
+                if (!Drawer.Exists) return;
+
+                Vector2 ci_in = new Vector2(x, y + innerRadius);
+                Vector2 ci_out = new Vector2(x, y - outerRadius);
+
+                Vector2 ci0_in = ci_in;
+                Vector2 ci0_out = ci_out;
+
+                Vector2 cil_in = ci_in;
+                Vector2 cil_out = ci_out;
+
+                float s = Mathf.Sign(value);
+                float add = 0.3f * s;
+                float limit = Mathf.Abs(2 * Mathf.PI * value);
+
+                for (float theta = 0.0f; Mathf.Abs(theta) < limit; theta += add)
+                {
+
+                    ci_in = new Vector2(
+                        x + (Mathf.Sin(theta) * innerRadius),
+                        y - (Mathf.Cos(theta) * innerRadius));
+
+                    ci_out = new Vector2(
+                        x + (Mathf.Sin(theta) * outerRadius),
+                        y - (Mathf.Cos(theta) * outerRadius));
+
+                    screenTrisPoints.Add(cil_in);
+                    screenTrisPoints.Add(cil_out);
+                    screenTrisPoints.Add(ci_out);
+
+                    screenTrisPoints.Add(ci_out);
+                    screenTrisPoints.Add(ci_in);
+                    screenTrisPoints.Add(cil_in);
+
+                    // previous points
+                    cil_in = ci_in;
+                    cil_out = ci_out;
+                }
+
+
+
+                // last segment
+                ci_in = new Vector2(
+                    x + (Mathf.Sin(limit * s) * innerRadius),
+                    y - (Mathf.Cos(limit * s) * innerRadius));
+
+                ci_out = new Vector2(
+                    x + (Mathf.Sin(limit * s) * outerRadius),
+                    y - (Mathf.Cos(limit * s) * outerRadius));
+
+                screenTrisPoints.Add(cil_in);
+                screenTrisPoints.Add(cil_out);
+                screenTrisPoints.Add(ci_out);
+
+                screenTrisPoints.Add(ci_out);
+                screenTrisPoints.Add(ci_in);
+                screenTrisPoints.Add(cil_in);
+            }
+        }
+    }
+}
---
+++ NDraw/DrawSlider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NDraw
+{
+    public static partial class Draw
+    {
+        public static partial class Screen
+        {
+            public static void Slider(float value, int x, int y, Color color, int width)
+            {
+                if (!Drawer.Exists) return;
+
+                value = Mathf.Clamp01(value);
+
+                Color c = new Color(1, 1, 1, 0.2f);
+
+                SetFillColor(c);
+                FillRect(x, y, width, 10);
+
+                SetFillColor(color);
+                FillRect(x, y, (int)(value * width), 10);
+            }
+
+            public static void MidSlider(float value, int x, int y, Color color, int width)
+            {
+                if (!Drawer.Exists) return;
+
+                value = Mathf.Clamp(value, -1, 1);
+
+                Color c = new Color(1, 1, 1, 0.2f);
+
+                SetFillColor(c);
+                FillRect(x, y, width, 10);
+
+                SetFillColor(color);
+                FillRect(x + width * 0.5f, y, (int)(value * 0.5f * width), 10);
+            }
+        }
+    }
+}
