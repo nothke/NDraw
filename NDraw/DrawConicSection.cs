d750cdff5e0c5a052bfbff181805c3cec1d804dc
Ivan Notaros <nothke@gmail.com>
2019-09-06 01:28:17 +0200

    Added conic section drawing (for orbits) and added to example

---
+++ NDraw/DrawConicSection.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NDraw
+{
+    public static partial class Draw
+    {
+        public static partial class World
+        {
+            /// <summary>
+            /// Draw a conic section, oriented with periapsis at forward.
+            /// Draws a circle, ellipse, parabola or hyperbola depending on the eccentricity.
+            /// It does not draw the negative part of hyperbola.
+            /// </summary>
+            /// <param name="center">The focus point</param>
+            /// <param name="eccentricity">e=0 is circle, e less than 1 is ellipse, e=1 is parabola, e more than 1 is hyperbola </param>
+            /// <param name="semiMajorAxis">Semi major axis of the ellipse, in case e is more than 1 it should be negative</param>
+            /// <param name="normal">The normal vector of the section</param>
+            /// <param name="forward">The direction of the periapsis</param>
+            public static void ConicSection(Vector3 center, float eccentricity, float semiMajorAxis, Vector3 normal, Vector3 forward, int interpolations)
+            {
+                float semilatus = eccentricity == 1 ? semiMajorAxis :
+                    semiMajorAxis * (1 - eccentricity * eccentricity);
+
+                if (semilatus <= 0) return;
+                if (interpolations <= 0) interpolations = 10;
+                if (eccentricity < 0) eccentricity = 0;
+
+                forward = Vector3.ProjectOnPlane(forward, normal).normalized;
+                Vector3 right = Vector3.Cross(forward, normal).normalized;
+
+                Vector3 prevlp = new Vector3();
+                Vector3 prevrp = new Vector3();
+
+                int num = interpolations;
+                float thetadiff = Mathf.PI / num;
+                float theta = 0;
+
+                bool breakn = false;
+
+                for (int i = 0; i < num + 1; i++)
+                {
+                    float cosTheta = Mathf.Cos(theta);
+                    float r = semilatus / (1 + eccentricity * cosTheta);
+
+                    if (r < 0) { r *= -100; breakn = true; }
+
+                    Vector3 rvec = right * Mathf.Sin(theta) * r;
+                    Vector3 fvec = forward * cosTheta * r;
+
+                    // Left side
+                    Vector3 lp = center - rvec + fvec;
+
+                    if (theta != 0) worldPoints.Add(prevlp);
+                    worldPoints.Add(lp);
+                    prevlp = lp;
+
+                    // Right side
+                    Vector3 rp = center + rvec + fvec;
+
+                    if (theta != 0) worldPoints.Add(prevrp);
+                    worldPoints.Add(rp);
+                    prevrp = rp;
+
+                    theta += thetadiff;
+
+                    if (breakn) break; // Prevents drawing the negative part of hyperbola
+                }
+            }
+
+            /// <summary>
+            /// Draws an elliptic orbit in world space using periapsis and apoapsis
+            /// </summary>
+            public static void ConicSectionUsingApses(Vector3 center, float periapsis, float apoapsis, Vector3 normal, Vector3 forward, int interpolations)
+            {
+                float a = (periapsis + apoapsis) / 2;
+                float e = (apoapsis - periapsis) / (apoapsis + periapsis);
+
+                // TODO: Add interpolations
+                ConicSection(center, e, a, normal, forward, interpolations);
+            }
+        }
+    }
+}
--- NDrawExample/NDrawExample.cs
+++ NDrawExample/NDrawExample.cs
@@ -4,6 +4,9 @@ using NDraw;
 
 public class NDrawExample : MonoBehaviour
 {
+    public bool screenExamples = true;
+    public bool worldExamples = true;
+
     Vector2[] multilinePoints;
 
     void Start()
@@ -18,63 +21,75 @@ public class NDrawExample : MonoBehaviour
 
     void Update()
     {
-        Draw.Screen.Line(140, 10, 240, 200);
-        Draw.Screen.SetColor(Color.black);
-        Draw.Screen.Line(new Vector2(150, 10), new Vector2(250, 200));
+        if (screenExamples)
+        {
+            Draw.Screen.Line(140, 10, 240, 200);
+            Draw.Screen.SetColor(Color.black);
+            Draw.Screen.Line(new Vector2(150, 10), new Vector2(250, 200));
 
-        Draw.Screen.SetColor(Color.blue);
-        Draw.Screen.Ellipse(new Vector2(500, 500), new Vector2(100, 200));
+            Draw.Screen.SetColor(Color.blue);
+            Draw.Screen.Ellipse(new Vector2(500, 500), new Vector2(100, 200));
 
-        Draw.Screen.SetColor(Color.cyan);
-        Draw.Screen.Ellipse(new Vector2(550, 500), new Vector2(130, 200));
+            Draw.Screen.SetColor(Color.cyan);
+            Draw.Screen.Ellipse(new Vector2(550, 500), new Vector2(130, 200));
 
-        Draw.Screen.SetColor(Color.yellow);
-        Draw.Screen.Rect(10, 10, 100, 200);
-        Draw.Screen.SetColor(Color.cyan);
-        Draw.Screen.Rect(new Rect(20, 20, 100, 200));
+            Draw.Screen.SetColor(Color.yellow);
+            Draw.Screen.Rect(10, 10, 100, 200);
+            Draw.Screen.SetColor(Color.cyan);
+            Draw.Screen.Rect(new Rect(20, 20, 100, 200));
 
-        Draw.Screen.SetColor(Color.red);
-        Draw.Screen.MultiLine(multilinePoints);
+            Draw.Screen.SetColor(Color.red);
+            Draw.Screen.MultiLine(multilinePoints);
 
-        Draw.Screen.SetColor(Color.yellow);
-        Draw.Screen.Circle(600, 230, 200);
+            Draw.Screen.SetColor(Color.yellow);
+            Draw.Screen.Circle(600, 230, 200);
 
-        Draw.Screen.Grid(10, 10, new Rect(600, 100, 250, 200));
+            Draw.Screen.Grid(10, 10, new Rect(600, 100, 250, 200));
 
-        Draw.Screen.SetFillColor(new Color(0, 1, 0, 0.1f));
-        Draw.Screen.FillRect(500, 500, 100, 200);
+            Draw.Screen.SetFillColor(new Color(0, 1, 0, 0.1f));
+            Draw.Screen.FillRect(500, 500, 100, 200);
 
-        Draw.Screen.SetFillColor(new Color(0, 1, 1, 0.1f));
-        Draw.Screen.FillRect(100, 500, 100, 200);
+            Draw.Screen.SetFillColor(new Color(0, 1, 1, 0.1f));
+            Draw.Screen.FillRect(100, 500, 100, 200);
 
-        float pieValue = Mathf.Sin(Time.time / 2) * 2;
-        if (pieValue < 0)
-            Draw.Screen.SetFillColor(new Color(1, 0, 0, 0.5f));
-        else Draw.Screen.SetFillColor(new Color(1, 0.5f, 0, 0.5f));
+            float pieValue = Mathf.Sin(Time.time / 2) * 2;
+            if (pieValue < 0)
+                Draw.Screen.SetFillColor(new Color(1, 0, 0, 0.5f));
+            else Draw.Screen.SetFillColor(new Color(1, 0.5f, 0, 0.5f));
 
-        Draw.Screen.Pie(200, 200, 20, 40, pieValue);
-        // outline test
-        //Draw.Screen.SetFillColor(Color.black);
-        //Draw.Screen.Pie(200, 200, 19, 20, pieValue);
-        //Draw.Screen.Pie(200, 200, 40, 41, pieValue);
+            Draw.Screen.Pie(200, 200, 20, 40, pieValue);
+            // outline test
+            //Draw.Screen.SetFillColor(Color.black);
+            //Draw.Screen.Pie(200, 200, 19, 20, pieValue);
+            //Draw.Screen.Pie(200, 200, 40, 41, pieValue);
 
-        Draw.Screen.Pie(200, 200, 45, 50, -pieValue);
+            Draw.Screen.Pie(200, 200, 45, 50, -pieValue);
 
-        Draw.Screen.Slider(pieValue * 0.5f, 200, 50, Color.red, 200);
-        Draw.Screen.MidSlider(pieValue * 0.5f, 200, 60, Color.blue, 200);
+            Draw.Screen.Slider(pieValue * 0.5f, 200, 50, Color.red, 200);
+            Draw.Screen.MidSlider(pieValue * 0.5f, 200, 60, Color.blue, 200);
+        }
 
         // 3D
 
-        Draw.World.Line(Vector3.zero, Vector3.forward * 100);
+        if (worldExamples)
+        {
+            Draw.World.Line(Vector3.zero, Vector3.forward * 100);
 
-        Draw.World.SetColor(Color.magenta);
-        Draw.World.Cube(Vector3.zero, Vector3.one * 10, Vector3.forward, Vector3.up);
+            Draw.World.SetColor(Color.magenta);
+            Draw.World.Cube(Vector3.zero, Vector3.one * 10, Vector3.forward, Vector3.up);
 
-        Draw.World.SetColor(Color.red);
-        Draw.World.Cube(Vector3.zero, Vector3.one, Vector3.forward, Vector3.up);
+            Draw.World.SetColor(Color.red);
+            Draw.World.Cube(Vector3.zero, Vector3.one, Vector3.forward, Vector3.up);
 
-        Draw.World.Circle(Vector3.forward * 5, 10, Vector3.up);
-        Draw.World.SetColor(Color.cyan);
-        Draw.World.Circle(Vector3.right * 5, 10, Vector3.up);
+            Draw.World.Circle(Vector3.forward * 5, 10, Vector3.up);
+            Draw.World.SetColor(Color.cyan);
+            Draw.World.Circle(Vector3.right * 5, 10, Vector3.up);
+
+            Draw.World.SetColor(new Color(0, 1, 0, 0.4f));
+            for (float e = 0.1f; e < 1.6f; e += 0.1f)
+            {
+                Draw.World.ConicSectionUsingApses(new Vector3(1, 1, 1), 1, e * 10, Vector3.forward, Vector3.right, 40);
+            }
+        }
     }
 }
